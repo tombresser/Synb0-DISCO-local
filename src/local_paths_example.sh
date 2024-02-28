04434c6789efb25c8274717819fd1ce8dab62f40
Tom <tom_bresser@hotmail.com>
2024-02-28 14:25:26 +0100

    added URLs to installation guides

--- src/local_paths_example.sh
+++ src/local_paths_example.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-# This file is used to setup the local paths to the required toolboxes.
+# This is an example file with installation guide URLS and how to setup the local paths to the required toolboxes.
 # Plese make sure that all required tools are installed and the paths are set 
-# correctly in a copy of this file named 'local_paths.sh'.
+# correctly in a copy of this file named 'src/local_paths.sh'.
 #
 # Installation guides:
-# Freesurfer: https://surfer.nmr.mgh.harvard.edu/fswiki/DownloadAndInstall
-# FSL: https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FslInstallation
-# ANTS: https://github.com/ANTsX/ANTs
+# Freesurfer: 
+#    - https://surfer.nmr.mgh.harvard.edu/fswiki/DownloadAndInstall
+# FSL: 
+#    - https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FslInstallation
+# ANTS: 
+#    - https://github.com/ANTsX/ANTs
 #    - local compiling https://github.com/ANTsX/ANTs/wiki/Compiling-ANTs-on-Linux-and-Mac-OS
-#    - 
-# C3D: https://sourceforge.net/projects/c3d/
+# C3D: 
+#    - https://sourceforge.net/projects/c3d/
 #    - version 1.0.0 is also available at https://sourceforge.net/projects/c3d/files/c3d/1.0.0
-# PyTorch: https://pytorch.org/get-started/locally/
-#    - or install using the terminal: pip3 install torch torchvision torchaudio nibabel numpy scipy
+# PyTorch: 
+#    - https://pytorch.org/get-started/locally/
+#    - or install using the terminal: `pip3 install torch torchvision torchaudio nibabel numpy scipy`
 
 
 #-- Local paths --#
